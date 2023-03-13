baff64fcf258cc1ec6d9a2825edae9506a30b485
francope41 <francoeulises@yahoo.com.co>
2023-03-13 13:30:19 -0500

    Simple decaf working, half Functions.decaf working

--- utils.py
+++ utils.py
@@ -29,10 +29,10 @@ class Parser:
         "Program : Decl+"
         print("\n   Program: ")
         self.Decl()
-        if self.curr_token is None:
-            pass
-        else:
+        if self.curr_token is not None:
             self.Decl()
+        else:
+            print("ERROR")
 
     def Decl(self):
         "Decl : VariableDecl | FunctionDecl"
@@ -56,7 +56,6 @@ class Parser:
         if self.curr_token[0] ==";":
                 print("  {}   VarDecl: ".format(self.curr_token[2]))
                 self.Variable(first_tkn,next_tkn)
-                self.Next()
         else:
             raise Exception()
         
@@ -76,11 +75,24 @@ class Parser:
             print("         (return type) Type: {}".format(first_tkn[0]))
             print("  {}      Identifier: {}".format(next_tkn[2], next_tkn[0]))
             self.Formals()
+            if self.curr_token[0] == ")":
+                self.Next()
+                if self.curr_token[0] =="{":
+                    self.StmtBlock()
+                else:
+                    print("Syntax func {")    
 
-    def Variable(self,first_tkn,next_tkn):
-        print("         Type: {}".format(first_tkn[0]))
-        print("  {}      Identifier: {}".format(next_tkn[2],next_tkn[0]))
-        
+            else:
+                print("Syntax func )")    
+            
+    def Variable(self,first_tkn,next_tkn, t=0):
+        if t == 0:
+            print("         Type: {}".format(first_tkn[0]))
+            print("  {}      Identifier: {}".format(next_tkn[2],next_tkn[0]))
+        elif t == 1:
+            print("            Type: {}".format(first_tkn[0]))
+            print("  {}         Identifier: {}".format(next_tkn[2],next_tkn[0]))
+        self.Next()
 
     def GetType(self):
         if (self.curr_token is not None and self.curr_token[1] == "T_Void" or self.curr_token[1] == "T_Int" or self.curr_token[1] == "T_Double" or self.curr_token[1] == "T_String" 
@@ -91,8 +103,11 @@ class Parser:
 
     def Formals(self):
         'Formals : Variable+, | ϵ'
-        #self.Variable(first_tkn,next_tkn)
-        print("self", self.curr_token)
+        print("  {}      (formals) VarDecl: ".format(self.curr_token[2]))
+        first_tkn = self.curr_token
+        self.Next()
+        next_tkn = self.curr_token
+        self.Variable(first_tkn, next_tkn, t=1)
 
     def StmtBlock (self):
         "StmtBlock : { VariableDecl* Stmt* }"
@@ -105,9 +120,10 @@ class Parser:
         self.Next()
         curr_statement = self.curr_token
         if curr_statement[0] in statemen_types:
-            print("            {}: ".format(self.curr_token[0]+"Stmt"))
+            formating = str.replace(curr_statement[0],curr_statement[0][0],(curr_statement[0][0]).upper(),1)
+            #print("            {}: ".format(formating+"Stmt"))
             if curr_statement[0] in statemen_types: #PrintStmt
-                Statement_Type = methodcaller(str(curr_statement[0]+"Stmt"))
+                Statement_Type = methodcaller(str(formating+"Stmt"))
                 Statement_Type(self)
         
             else:
@@ -118,12 +134,18 @@ class Parser:
         pass
 
     def PrintStmt(self):
+        print("            {}: ".format("PrintStmt"))
         self.Next()
-
         if self.curr_token[0] == "(":
             self.Next() 
             print("  {}            (args) StringConstant: {}".format(self.curr_token[2],self.curr_token[0]))
 
+    def ReturnStmt(self):
+        print("  {}         ReturnStmt: ".format(self.curr_token[2]))
+        self.Next()
+        if self.curr_token[0] == "(":
+            self.Next() #FIGURE OUT ARITMETIC EXPRESION
+            
 
     def Parse(self):
         if self.curr_token is None:
